5c542fb151760ce319a2f4192bf224a332ef692a
Bleu404 <42571708+Bleu404@users.noreply.github.com>
2021-10-01 17:16:13 +0800

    Update 一键复制美剧链接.user.js

--- 一键复制美剧链接.user.js
+++ 一键复制美剧链接.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         一键复制美剧链接
-// @version      1.6.5
+// @version      1.6.6
 // @description  识别常用网址的美剧链接，美剧天堂添加跳转豆瓣、IMDB入口，原始匹配网站云盘可以直接进入，可识别用户匹配网站中的链接。附本人写的字幕翻译程序，下载地址在附加信息中。
 // @author       Bleu
 // @compatible   edge Tampermonkey
 // @license      MIT
 // @supportURL   https://greasyfork.org/zh-CN/scripts/430126-一键复制美剧链接/feedback
-// @match        https://*.meijutt.tv/*
+// @match        https://*.meijutt.tv/content/*
 // @match        https://www.22tu.tv/*
 // @match        https://www.kpkuang.com/*
 // @match        https://www.mp4er.com/*
@@ -271,6 +271,7 @@
                 pswd = '';
 
             if (pageURL.indexOf('bd2020') >= 0) {
+                localStorage.setItem("openid", 'omcj56pIE_ssXEG7s04uZ9saaa'); //去二维码
                 let hide = document.querySelector('.layui-icon.layui-icon-down');
                 hide ? hide.click() : 1;
                 linkParent = document.querySelectorAll('.option.copybtn.alert');
@@ -493,9 +494,12 @@
             let icon = '<div class="ico_bleu" id="douban"><svg height="24" viewBox="0 0 152 152" width="24" xmlns="http://www.w3.org/2000/svg"><g id="Layer_2" data-name="Layer 2"><g id="Color_Icon" data-name="Color Icon"><g id="_74.Douban" data-name="74.Douban"><rect id="Background" fill="#4caf50" height="152" rx="12" width="152"/><g id="Icon" fill="#fff"><path d="m95.91 107.15 5.6-19.15h7.87v-33.51h-66.7v33.51h7l6 19.14h-19.68v7.6h79.65v-7.6zm-39.8-44h40.25v16h-40.25zm28.53 44h-17.72l-5.92-19.15h29.24z"/><path d="m36.37 37.25h79.63v8.81h-79.63z"/></g></g></g></g></svg></div><div class="ico_bleu" id="imdb"><svg id="home_img" class="ipc-logo" xmlns="http://www.w3.org/2000/svg" width="48" height="24" viewBox="0 0 64 32" version="1.1"><g fill="#F5C518"><rect x="0" y="0" width="100%" height="100%" rx="4"></rect></g><g transform="translate(8.000000, 7.000000)" fill="#000000" fill-rule="nonzero"><polygon points="0 18 5 18 5 0 0 0"></polygon><path d="M15.6725178,0 L14.5534833,8.40846934 L13.8582008,3.83502426 C13.65661,2.37009263 13.4632474,1.09175121 13.278113,0 L7,0 L7,18 L11.2416347,18 L11.2580911,6.11380679 L13.0436094,18 L16.0633571,18 L17.7583653,5.8517865 L17.7707076,18 L22,18 L22,0 L15.6725178,0 Z"></path><path d="M24,18 L24,0 L31.8045586,0 C33.5693522,0 35,1.41994415 35,3.17660424 L35,14.8233958 C35,16.5777858 33.5716617,18 31.8045586,18 L24,18 Z M29.8322479,3.2395236 C29.6339219,3.13233348 29.2545158,3.08072342 28.7026524,3.08072342 L28.7026524,14.8914865 C29.4312846,14.8914865 29.8796736,14.7604764 30.0478195,14.4865461 C30.2159654,14.2165858 30.3021941,13.486105 30.3021941,12.2871637 L30.3021941,5.3078959 C30.3021941,4.49404499 30.272014,3.97397442 30.2159654,3.74371416 C30.1599168,3.5134539 30.0348852,3.34671372 29.8322479,3.2395236 Z"></path><path d="M44.4299079,4.50685823 L44.749518,4.50685823 C46.5447098,4.50685823 48,5.91267586 48,7.64486762 L48,14.8619906 C48,16.5950653 46.5451816,18 44.749518,18 L44.4299079,18 C43.3314617,18 42.3602746,17.4736618 41.7718697,16.6682739 L41.4838962,17.7687785 L37,17.7687785 L37,0 L41.7843263,0 L41.7843263,5.78053556 C42.4024982,5.01015739 43.3551514,4.50685823 44.4299079,4.50685823 Z M43.4055679,13.2842155 L43.4055679,9.01907814 C43.4055679,8.31433946 43.3603268,7.85185468 43.2660746,7.63896485 C43.1718224,7.42607505 42.7955881,7.2893916 42.5316822,7.2893916 C42.267776,7.2893916 41.8607934,7.40047379 41.7816216,7.58767002 L41.7816216,9.01907814 L41.7816216,13.4207851 L41.7816216,14.8074788 C41.8721037,15.0130276 42.2602358,15.1274059 42.5316822,15.1274059 C42.8031285,15.1274059 43.1982131,15.0166981 43.281155,14.8074788 C43.3640968,14.5982595 43.4055679,14.0880581 43.4055679,13.2842155 Z"></path></g></svg></div>';
             GM_addStyle('.ico_bleu{display: inline-flex;vertical-align: middle;margin: inherit;margin-left: 10px;}');
             GM_addStyle('.ico_bleu:active{box-shadow: 0 5px #666;transform: translateY(4px);}');
+            let icon1 = '<div class="ico_bleu" id="subscene"><img style="width: 24px;height: 24px;" src="https://subscene.com/favicon.ico"/></div>';
+            let icon2 = '<div class="ico_bleu" id="rarbg"><img style="width: 24px;height: 24px;" src="https://rarbgmirror.com/favicon.ico"/></div>';
+            let icon3 = '<div class="ico_bleu" id="piratebay"><img style="width: 24px;height: 24px;" src="https://piratebay.live/static/img/tpblogo_sm_ny.gif"/></div>';
             let IMDBName = OriginalName.substr(0, OriginalName.indexOf("season"));
             IMDBName ? IMDBName : IMDBName = OriginalName;
-            $('.info-title').append(icon);
+            $('.info-title').append(icon, icon1,icon2,icon3);
             $('#douban').on('click', function () {
                 GM_xmlhttpRequest({
                     url: 'https://movie.douban.com/j/subject_suggest?q=' + IMDBName.replace(/\s/g, '+'),
@@ -550,6 +554,32 @@
                         GM_openInTab('https://www.imdb.com/find?q=' + IMDBName)
                     }
                 })
+                IMDBName = IMDBName.replace(/_/g, ' ');
+            });
+            $('#subscene').on('click', function () {
+                var PARAMS = {query: IMDBName,l: ''};
+                var temp_form = document.createElement("form");
+                temp_form.action = 'https://subscene.com/subtitles/searchbytitle';
+                temp_form.target = "_blank";
+                temp_form.method = "post";
+                temp_form.style.display = "none";
+                for (var x in PARAMS) {
+                    var opt = document.createElement("textarea");
+                    opt.name = x;
+                    opt.value = PARAMS[x];
+                    temp_form.appendChild(opt);
+                }
+                document.body.appendChild(temp_form);
+                temp_form.submit();
+                document.body.removeChild(temp_form);
+            });
+            $('#rarbg').on('click', function () {
+                var rarbgURL = 'https://rarbgmirror.com/torrents.php?search=' + IMDBName;
+                GM_openInTab(encodeURI(rarbgURL));
+            });
+            $('#piratebay').on('click', function () {
+                var piratebayURL = 'https://piratebay.live/search/' + IMDBName;
+                GM_openInTab(encodeURI(piratebayURL));
             });
         },
         init() {
